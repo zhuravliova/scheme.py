4a0c16b0b4ddbffc0b1ea8c2e6a20dab8724816d
zhuravliova <sombrrre@gmail.com>
2019-08-01 18:08:36 +0300

    Parametrize test

--- test_lisp_int.py
+++ test_lisp_int.py
@@ -2,12 +2,9 @@ import pytest
 
 from lisp_int import parse
 
-
-def test_parse_works_correctly():
-    assets = {
-        '(+ 2 (* 2 2))': ['+', 2, ['*', 2, 2]],
-        '(define (size 2))': ['define', ['size', 2]],
-        '(+ (* 5 3) (/ 8 4))': ['+', ['*', 5, 3], ['/', 8, 4]]
-    }
-    for src, result in assets.items():
-        assert parse(src) == result
+@pytest.mark.parametrize("src, result",
+                         [('(+ 2 (* 2 2))', ['+', 2, ['*', 2, 2]]),
+                          ('(define (size 2))', ['define', ['size', 2]]),
+                          ('(+ (* 5 3) (/ 8 4))', ['+', ['*', 5, 3], ['/', 8, 4]])])
+def test_parse_works_correctly(src, result):
+    assert parse(src) == result
